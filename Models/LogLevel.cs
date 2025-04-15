7f72cdf9a0ecf27c1efa245bfbee0e5d1e546b83
Ryabov Daniil [NotACat] <113008873+NotACat1@users.noreply.github.com>
2025-04-15 14:27:17 +0300

    feat: Создание уровней логов

---
+++ Models/LogLevel.cs
@@ -0,0 +1,12 @@
+namespace WeatherTerminal.Models;
+
+/// <summary>
+/// Уровни логирования для системы логгирования
+/// </summary>
+public enum LogLevel
+{
+    DEBUG,      // Отладочная информация
+    INFO,       // Информационные сообщения
+    WARNING,    // Предупреждения
+    ERROR       // Критические ошибки
+}
